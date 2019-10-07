ce0a5880da8cf7d39c812a3bec16e544690339ba
Robin Moffatt <robin@confluent.io>
2019-10-07 11:09:24 +0100

    Move curl response into variable

--- cp-ksql-server/include/etc/confluent/docker/healthcheck.sh
+++ cp-ksql-server/include/etc/confluent/docker/healthcheck.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ $(curl -s -o /dev/null -w %{http_code} $KSQL_LISTENERS/info) = 200 ]]; then
+curl_status=$(curl -s -o /dev/null -w %{http_code} $KSQL_LISTENERS/info) 
+if [ $curl_status -eq 200 ]; then
   echo "Woohoo! KSQL is up!"
   exit 0
-else 
-  echo -e $(date) "\tKSQL HTTP state: " $(curl -s -o /dev/null -w %{http_code} $KSQL_LISTENERS/info) " (waiting for 200)"
+else
+  echo -e $(date) " KSQL server listener HTTP state: " $curl_status " (waiting for 200)" 
   exit 1
-fi
+fi 
