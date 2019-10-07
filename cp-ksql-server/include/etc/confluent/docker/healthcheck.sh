405389c97259f11395b28a3782884be3599342b0
Robin Moffatt <robin@confluent.io>
2019-10-07 11:17:22 +0100

    Use REST query for better status probe

--- cp-ksql-server/include/etc/confluent/docker/healthcheck.sh
+++ cp-ksql-server/include/etc/confluent/docker/healthcheck.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-curl_status=$(curl -s -o /dev/null -w %{http_code} $KSQL_LISTENERS/info) 
+curl_status=$(curl -X POST -s -o /dev/null -w %{http_code} $KSQL_LISTENERS/ksql -H 'content-type: application/vnd.ksql.v1+json; charset=utf-8' -d '{"ksql":"SHOW TOPICS;"}') 
 if [ $curl_status -eq 200 ]; then
   echo "Woohoo! KSQL is up!"
   exit 0
 else
-  echo -e $(date) " KSQL server listener HTTP state: " $curl_status " (waiting for 200)" 
+  echo -e $(date) " KSQL server query response code: " $curl_status " (waiting for 200)" 
   exit 1
 fi 
