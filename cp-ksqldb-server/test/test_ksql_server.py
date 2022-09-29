3d9e3a5571a7e695ac0f63c34180e2a103c2cf57
Alieh <107070585+aliehsaeedii@users.noreply.github.com>
2022-09-29 20:34:11 +0200

    Update test_ksql_server.py

--- cp-ksqldb-server/test/test_ksql_server.py
+++ cp-ksqldb-server/test/test_ksql_server.py
@@ -58,7 +58,7 @@ class KsqlClient(object):
         return self.request('/info').decode()
 
 
-def retry(op, timeout=30):
+def retry(op, timeout=600):
     start = time.time()
     while time.time() - start < timeout:
         try:
@@ -77,7 +77,7 @@ class KsqlServerTest(unittest.TestCase):
         cls.cluster.start()
         try:
             start = time.time()
-            while time.time() - start < 30:
+            while time.time() - start < 600:
                 if check_cluster_ready(cls.cluster):
                     return
             assert check_cluster_ready(cls.cluster)
