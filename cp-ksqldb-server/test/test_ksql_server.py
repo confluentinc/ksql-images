7926fc055e2d8257e7846f68488d9189f50f02e0
Vedarth Sharma <vesharma@confluent.io>
2025-05-23 13:23:19 +0530

    [KSQL-13417] | Fix kafka configs

--- cp-ksqldb-server/test/test_ksql_server.py
+++ cp-ksqldb-server/test/test_ksql_server.py
@@ -56,7 +56,7 @@ class KsqlClient(object):
         return self.request('/info').decode()
 
 
-def retry(op, timeout=1200):
+def retry(op, timeout=600):
     start = time.time()
     while time.time() - start < timeout:
         try:
@@ -75,7 +75,7 @@ class KsqlServerTest(unittest.TestCase):
         cls.cluster.start()
         try:
             start = time.time()
-            while time.time() - start < 1200:
+            while time.time() - start < 600:
                 if check_cluster_ready(cls.cluster):
                     return
             assert check_cluster_ready(cls.cluster)
