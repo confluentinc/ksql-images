4284be5ee0a1e9d68f41b3c7b9c8716e7654cd9a
unicorn-io <vtayal@confluent.io>
2025-07-07 14:18:48 +0530

    KSQL-13572 Unify ksql docker images to a single image

---
+++ cp-ksqldb/include/etc/confluent/docker/entrypoint.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+#
+# Copyright 2024 Confluent Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+# Determine the mode based on the first argument
+MODE="${1:-server}"
+
+case "$MODE" in
+  server)
+    echo "===> Starting KSQLDB in server mode..."
+    export COMPONENT=ksqldb-server
+    exec /etc/confluent/docker-server/run
+    ;;
+  cli)
+    echo "===> Starting KSQLDB in CLI mode..."
+    export COMPONENT=ksqldb-cli
+    # Shift to remove the 'cli' argument and pass remaining args to ksql
+    shift
+    exec /etc/confluent/docker-cli/run "$@"
+    ;;
+  examples)
+    echo "===> Starting KSQLDB in examples mode..."
+    export COMPONENT=ksqldb-examples
+    # Change to root directory to avoid /home/appuser prefix issues
+    cd /
+    # For examples, we just run a shell or execute passed commands
+    shift
+    if [ $# -eq 0 ]; then
+      exec /bin/bash
+    else
+      # Use bash -c to handle all commands uniformly
+      exec /bin/bash -c "$*"
+    fi
+    ;;
+  *)
+    echo "Error: Unknown mode '$MODE'. Valid modes are: server, cli, examples"
+    echo ""
+    echo "Usage:"
+    echo "  Docker run with server mode (default):"
+    echo "    docker run <image>"
+    echo "    docker run <image> server"
+    echo ""
+    echo "  Docker run with CLI mode:"
+    echo "    docker run -it <image> cli [ksql-server-url]"
+    echo "    docker run -it <image> cli http://localhost:8088"
+    echo ""
+    echo "  Docker run with examples mode:"
+    echo "    docker run -it <image> examples"
+    echo "    docker run -it <image> examples [command]"
+    echo ""
+    echo "  Using --entrypoint override:"
+    echo "    docker run --entrypoint ksql-server-start <image> /etc/ksqldb-server/ksqldb-server.properties"
+    echo "    docker run -it --entrypoint ksql <image> http://localhost:8088"
+    echo "    docker run -it --entrypoint /bin/bash <image>"
+    exit 1
+    ;;
+esac
